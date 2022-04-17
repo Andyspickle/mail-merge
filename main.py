33e8d6d612eba5e44e994bd4266f2105d8de171c
Andrew Cliff <andyspickle@gmail.com>
2022-04-17 17:22:00 +1000

    added PLACEHOLDER

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 #Save the letters in the folder "ReadyToSend".
 STARTING_LETTER = "./Input/Letters/starting_letter.txt"
 INVITED_NAMES = "./Input/Names/invited_names.txt"
+PLACEHOLDER = "[name]"
+
 
 def read_file_lines(file: str):
     with open(file, "r") as f:
@@ -17,20 +19,16 @@ def write_file(file: str):
     with open(file, "r") as f:
         return f.readlines()
 
-def update_name(file: str, name: str):
-    with open(file, "r") as f:
-        opened_letter = read_file(file)
-        opened_letter.replace("[name]", name)
-        return opened_letter
+def update_name(opened_letter: str, name: str):
+    opened_letter.replace("[name]", name)
+    return opened_letter
+
 
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
 
+with open(INVITED_NAMES, "r") as names_files:
+    names = names_files.readlines()
 
-
-for name in read_file_lines(INVITED_NAMES):
-    letter = update_name(STARTING_LETTER, name)
-    print(name)
-    print(letter)
